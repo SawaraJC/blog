cd282bd2f6bac6f03d784a5e46aed1a00c606e67
SawaraJC <sawarakhg804@gmail.com>
2024-09-26 13:22:03 +0530

    Added custom error handlers for 404 and 500

--- hello.py
+++ hello.py
@@ -15,3 +15,17 @@ def index():
 def user(name):
     return render_template('user.html', user_name=name)
 
+
+#create custom error page
+
+#invalid url
+@app.errorhandler(404)
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+#internal server error
+@app.errorhandler(500)
+
+def internal_server_error(e):
+    return render_template('500.html'), 500
